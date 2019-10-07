b6a0b736ce3a9d4d89ba43109fc80658dfc2c08f
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-07 17:11:28 -0600

    Vehicles Controller coded

--- automaintainer-backend/app/controllers/vehicles_controller.rb
+++ automaintainer-backend/app/controllers/vehicles_controller.rb
@@ -1,16 +1,52 @@
 class VehiclesController < ApplicationController
+  
   def index
-  end
+    vehicles = Vehicle.all
 
-  def show
+    render json: Vehicle.all, include: [:maint_events]
   end
 
   def create
+    binding.pry
+    vehicle = Vehicle.new(vehicle_params)
+
+    if vehicle.save 
+      render json: vehicle
+    else
+      render json: { message: "Error - Missing Data: #{vehicle.error.full_messages.join(", ")}" }
+    end
+
+  end
+
+  def show
+    vehicle = set_vehicle
+
+    if vehicle
+      render json: vehicle, include: [:maint_events]
+    else
+      render json: { message: "Error - Vehicle not found" }
+    end
   end
 
   def update
+    vehicle = set_vehicle
+
+    if vehicle.update(vehicle_params)
+      render json: vehicle, include: [:maint_events]
+      #not sure I need to include maint_events on just updating the vehicle - REFACTOR
+    else
+      render json: { message: "Error - Vehicle not found" }
+    end
+
   end
 
 private 
-
+  def vehicle_params
+    params.require(:vehicle).permit(:year, :make, :model, :color, :owner, :vin)
+  end
+  
+  def set_vehicle
+    vehicle = Vehicle.find_by_id(params[:id])
+  end
+  
 end
