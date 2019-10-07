720d7f90ca80e55f5bf0d7d9937c8d7a3c33375e
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-07 17:20:46 -0600

    MaintEvents controller coded.

--- automaintainer-backend/app/controllers/maint_events_controller.rb
+++ automaintainer-backend/app/controllers/maint_events_controller.rb
@@ -1,13 +1,47 @@
 class MaintEventsController < ApplicationController
-  def show
+  
+  def create
+    maint_event = MaintEvent.new(maint_event_params)
+
+    if maint_event.save 
+      render json: maint_event
+    else
+      render json: { message: "Error(s): #{maint_event.error.full_messages.join(", ")}" }
+    end
+
   end
 
-  def create
+  def show
+    maint_event = set_maint_event
+
+    if maint_event
+      render json: maint_event, include: [:maint_events]
+    else
+      render json: { message: "Error - MaintEvent not found" }
+    end
+
   end
 
   def update
+    maint_event = set_maint_event
+    
+    if maint_event.update(maint_event_params)
+      render json: maint_event
+      #not sure I need to include maint_events on just updating the maint_event - REFACTOR
+    else
+      render json: { message: "Error(s): #{maint_event.error.full_messages.join(", ")}" }
+    end
+
   end
 
 private 
 
+  def maint_event_params
+    params.require(:maint_event).permit(:mileage, :completed, :event_type, :comment, :cost, :vehicle_id)
+  end
+  
+  def set_maint_event
+    MaintEvent.find_by_id(params[:id])
+  end
+  
 end
