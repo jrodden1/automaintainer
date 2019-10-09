1a801dc1f8b2d6543a6b26d7849901fcc11b3260
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-09 09:48:53 -0600

    fixed MaintEvent backend Controller bugs with processing completed date and rendering error messages

--- automaintainer-backend/app/controllers/maint_events_controller.rb
+++ automaintainer-backend/app/controllers/maint_events_controller.rb
@@ -1,12 +1,14 @@
 class MaintEventsController < ApplicationController
   
   def create
+    binding.pry
+    process_completed_date_param_to_date_obj
     maint_event = MaintEvent.new(maint_event_params)
 
     if maint_event.save 
       render json: maint_event, status: 201
     else
-      render json: { message: "Error(s): #{maint_event.error.full_messages.join(", ")}" }, status: 400
+      render json: { message: "Error(s): #{maint_event.errors.full_messages.join(", ")}" }, status: 400
     end
 
   end
@@ -29,7 +31,7 @@ class MaintEventsController < ApplicationController
       render json: maint_event, status: 201
       #not sure I need to include maint_events on just updating the maint_event - REFACTOR
     else
-      render json: { message: "Error(s): #{maint_event.error.full_messages.join(", ")}" }, status: 400
+      render json: { message: "Error(s): #{maint_event.errors.full_messages.join(", ")}" }, status: 400
     end
 
   end
@@ -39,6 +41,11 @@ private
   def maint_event_params
     params.require(:maint_event).permit(:mileage, :completed, :event_type, :comment, :cost, :vehicle_id)
   end
+
+  def process_completed_date_param_to_date_obj
+    params[:maint_event][:completed] = Date.strptime(params[:maint_event][:completed], '%m/%d/%Y') 
+  end
+  
   
   def set_maint_event
     MaintEvent.find_by_id(params[:id])
